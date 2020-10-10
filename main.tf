328a4ef8ec94d4aa8db4a851ac45b684cabfd8a7
Mark Ingram <mark@markdingram.com>
2020-10-10 13:31:03 +0100

    Cluster

--- main.tf
+++ main.tf
@@ -2,3 +2,42 @@ provider "google" {
   project = "ckad-cluster-mdi"
   region  = "europe-west1"
 }
+
+resource "google_container_cluster" "primary" {
+  name     = "ckad-cluster-mdi"
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "ckad-cluster-mdi-node-pool"
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
